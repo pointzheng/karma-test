c22beeabba4be88313caaba8875431085fff6220
zhengyy <zhengyy@calis.edu.cn>
2020-11-02 16:17:23 +0800

    add test case

---
+++ components/MyCompForTest.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class MyCompForTest extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log("component has mounted....");
+  }
+
+  render() {
+    const {foo, bar} = this.props;
+
+    if (foo) {
+      return <div id="foo">foo</div>
+    }
+    if (bar) {
+      return <div id="bar">bar</div>
+    }
+    return <div id="noFooBar">Not foo and not bar</div>
+  }
+}
--- test/util-test.js
+++ test/util-test.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Fixture from '../components/FixTure';
+import MyCompForTest from '../components/MyCompForTest';
 
 describe('MyComponent', () => {
   it('renders', () => {
     const wrapper = mount(<Fixture />)
     expect(wrapper.find('#disabled').hasClass("testClz")).toBe(true);
     expect(wrapper.find('#not').hasClass("testClz")).toBe(false);
+  })
+
+  it('testFooBar', () => {
+    const wrapper = mount(<MyCompForTest foo="3" />)
+    expect(wrapper.find('#foo').length).toBe(1);
   })
 })
