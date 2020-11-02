5c537bb16c1c8b39ecc2b97d74e00eb90b4a4be5
zhengyy <zhengyy@calis.edu.cn>
2020-11-02 16:05:21 +0800

    slpit biz components from test file

---
+++ components/FixTure.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Fixture() {
+  const a = 3;
+
+  // The following code is to test coverage rate for comparison
+  function inner() {
+    var b = 5;
+    console.log("b:", b)
+  }
+
+  return (
+    <div>
+       <input id="disabled" disabled className="testClz" />
+        <input id="not"/>
+    </div>
+  );
+}
--- test/util-test.js
+++ test/util-test.js
@@ -1,25 +1,10 @@
-// import jasmineEnzyme from 'jasmine-enzyme';
 import React from 'react'
-// import { shallow } from 'enzyme';
-// import {mount, shallow} from 'enzyme'
-// import { describe } from 'jasmine';
-// import { mount, shallow } from 'enzyme';
-// import { render } from 'enzyme';
-// import { shallow } from 'enzyme';
-
-function Fixture() {
-  return (
-    <div>
-       <input id="disabled" disabled className="testClz" />
-        <input id="not"/>
-    </div>
-  );
-}
+import Fixture from '../components/FixTure';
 
 describe('MyComponent', () => {
   it('renders', () => {
-    expect(true).toBe(true)
     const wrapper = mount(<Fixture />)
     expect(wrapper.find('#disabled').hasClass("testClz")).toBe(true);
+    expect(wrapper.find('#not').hasClass("testClz")).toBe(false);
   })
 })
